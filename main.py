339b7be7d47ba17217e692f5d7b64c5e0c16f19f
Caterina Migala <caterina.migala@gmail.com>
2025-02-03 19:14:39 +0100

    reload data

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ def parse(x):
     z=y[0] + " " + t
     d = datetime.strptime(z, '%Y-%m-%d %H:%M:%S')
     return d
+
+df = pd.read_excel(
+    'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/data-science-in-health-care-basic-statistical-analysis/COVID_19.xlsx',
+    'Sheet1',
+    na_values="NaN",
+    parse_dates=['Date time'],
+    index_col=0,
+    date_format=parse)
