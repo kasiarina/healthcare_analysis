ae82c27d4a213713433354ea7d5949541501df83
Caterina Migala <caterina.migala@gmail.com>
2025-02-03 18:31:15 +0100

    imported data

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 import openpyxl
 import pandas as pd
 from datetime import datetime
+#data from repository
+df = pd.read_excel(
+    'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/data-science-in-health-care-basic-statistical-analysis/COVID_19.xlsx',
+    'Sheet1')
+print(df.head)
