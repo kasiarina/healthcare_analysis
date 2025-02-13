7f5e0d9bfe87f3217953de1780fb895c1cd1fc50
Caterina Migala <caterina.migala@gmail.com>
2025-02-13 18:19:51 +0100

    refactor: clean up code formatting and improve readability

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
-#import necessary libraries
+# import necessary libraries
 import openpyxl
 import pandas as pd
 from datetime import datetime
-#data from repository
+
+# data from repository
 df = pd.read_excel(
-    'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/data-science-in-health-care-basic-statistical-analysis/COVID_19.xlsx',
-    'Sheet1')
+    "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/data-science-in-health-care-basic-statistical-analysis/COVID_19.xlsx",
+    "Sheet1",
+)
 print(df.head)
-#function to transform date to datetime
+
+
+# function to transform date to datetime
 def parse(x):
     y = x.split()
     t = y[1][:8]
-    z=y[0] + " " + t
-    d = datetime.strptime(z, '%Y-%m-%d %H:%M:%S')
+    z = y[0] + " " + t
+    d = datetime.strptime(z, "%Y-%m-%d %H:%M:%S")
     return d
-#reload data
-df = pd.read_excel(
-    'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/data-science-in-health-care-basic-statistical-analysis/COVID_19.xlsx',
-    'Sheet1',
-    na_values="NaN",
-    parse_dates=['Date time'],
-    index_col=0,
-    date_format=parse)
 
-#drop null values for gender column
-df = df.dropna(subset=['Gender'])
-#change yes/no values
-d = {'No' : False, 'Yes' : True}
-c = 'Do you vaccinated influenza?'
+
+# reload data
+df = pd.read_excel(
+    "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/data-science-in-health-care-basic-statistical-analysis/COVID_19.xlsx",
+    "Sheet1",
+    na_values="NaN",
+    parse_dates=["Date time"],
+    index_col=0,
+    date_format=parse,
+)
+
+# drop null values for gender column
+df = df.dropna(subset=["Gender"])
+# change yes/no values
+d = {"No": False, "Yes": True}
+c = "Do you vaccinated influenza?"
 df.loc[:, c] = df[c].map(d)
-#overview of dataset
+# overview of dataset
 df.info()
-#change column type
-c = 'Age'
-df.loc[:, c] = df[c].astype('category')
+# change column type
+c = "Age"
+df.loc[:, c] = df[c].astype("category")
